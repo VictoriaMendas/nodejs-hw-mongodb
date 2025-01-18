2cdcba12cf470f1c4ff3b1ed406c7a711f6d4fee
VictoriaMendas <124498217+VictoriaMendas@users.noreply.github.com>
2025-01-18 13:09:42 +0100

    #2

--- src/db/initMongoConnection.js
+++ src/db/initMongoConnection.js
@@ -1,7 +1,6 @@
 import mongoose from 'mongoose';
 
 // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');`
-// if your database has auth enabled
 
 export const initMongoConnection = async () => {
   try {
