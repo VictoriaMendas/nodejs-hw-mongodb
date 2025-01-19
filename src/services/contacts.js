45d37e16d21269acf77e7f0af7dd46ff4026341c
VictoriaMendas <124498217+VictoriaMendas@users.noreply.github.com>
2025-01-19 15:00:21 +0100

    #5

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,36 +1,4 @@
-import { Schema, model } from 'mongoose';
-
-const contactsSchema = new Schema(
-  {
-    name: {
-      type: String,
-      required: true,
-    },
-    phoneNumber: {
-      type: String,
-      required: true,
-    },
-    email: {
-      type: String,
-      required: false,
-    },
-    isFavourite: {
-      type: Boolean,
-      required: false,
-      default: false,
-    },
-    contactType: {
-      type: String,
-      enum: ['work', 'home', 'personal'],
-      required: true,
-      default: 'personal',
-    },
-  },
-  {
-    timestamps: true,
-  },
-);
-export const ContactsCollection = model('contacts', contactsSchema);
+import { ContactsCollection } from '../db/models/contacts';
 
 export const getAllStudents = async () => {
   const contacts = await ContactsCollection.find();
