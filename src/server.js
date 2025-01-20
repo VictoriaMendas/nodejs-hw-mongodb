f9a085d7c79bbc1e2b3f863fd1f227abf593e18a
VictoriaMendas <124498217+VictoriaMendas@users.noreply.github.com>
2025-01-20 20:40:18 +0100

    template

--- src/server.js
+++ src/server.js
@@ -48,23 +48,24 @@ export const setupServer = () => {
     res.json({
       status: 200,
       message: `Successfully found contact with id ${contactId}!`,
-            data: getContactId,
-          });
-   
-  app.use('*', (req, res, next) => {
-    res.status(404).json({
-      message: 'Not found',
+      data: getContactId,
     });
-    next();
-  });
 
-  app.use((err, req, res, next) => {
-    res.status(500).json({
-      message: 'Something went wrong',
-      error: err.message,
+    app.use('*', (req, res, next) => {
+      res.status(404).json({
+        message: 'Not found',
+      });
+      next();
     });
-  });
-  app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+
+    app.use((err, req, res, next) => {
+      res.status(500).json({
+        message: 'Something went wrong',
+        error: err.message,
+      });
+    });
+    app.listen(PORT, () => {
+      console.log(`Server is running on http://localhost:${PORT}`);
+    });
   });
 };
