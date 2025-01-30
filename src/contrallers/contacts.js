bdaa1f324df04bca81a13e55b142a8ee62dac99b
VictoriaMendas <124498217+VictoriaMendas@users.noreply.github.com>
2025-01-30 12:25:25 +0100

    #3

--- src/contrallers/contacts.js
+++ src/contrallers/contacts.js
@@ -6,9 +6,11 @@ import {
   getContactById,
   updateContactById,
 } from '../services/contacts.js';
+import { parsePaginationParams } from '../utils/parsePaginationParams.js';
 
 export const getContactsController = async (req, res) => {
-  const contacts = await getAllContacts();
+  const { page, perPage } = parsePaginationParams(req.query);
+  const contacts = await getAllContacts({ page, perPage });
 
   res.json({
     status: 200,
--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,8 +1,25 @@
 import { ContactsCollection } from '../db/models/contacts.js';
+import { calculatePaginationData } from '../utils/calculatePaginationData.js';
 
-export const getAllContacts = async () => {
-  const contacts = await ContactsCollection.find();
-  return contacts;
+export const getAllContacts = async ({ page, perPage }) => {
+  const limit = perPage;
+  const skip = (page - 1) * perPage;
+
+  const contactsQuery = ContactsCollection.find();
+  const contactsCount = await ContactsCollection.find()
+    .merge(contactsQuery)
+    .countDocuments();
+
+  const students = await contactsQuery.skip(skip).limit(limit).exec();
+
+  const paginationData = calculatePaginationData(contactsCount, perPage, page);
+
+  return {
+    data: students,
+    ...paginationData,
+  };
+  // const contacts = await ContactsCollection.find();
+  // return contacts;
 };
 export const getContactById = async (contactId) => {
   const contact = await ContactsCollection.findById(contactId);
