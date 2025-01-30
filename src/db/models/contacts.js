3c4f99f901b7a4a173ecaf1f33e84bfd0f0ccb32
VictoriaMendas <124498217+VictoriaMendas@users.noreply.github.com>
2025-01-30 11:35:45 +0100

    fixed again

--- src/db/models/contacts.js
+++ src/db/models/contacts.js
@@ -28,6 +28,7 @@ const contactsSchema = new Schema(
   },
   {
     timestamps: true,
+    versionKey: false,
   },
 );
 export const ContactsCollection = model('contacts', contactsSchema);
--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -12,8 +12,8 @@ import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 const router = Router();
 
 router.get('/contacts', ctrlWrapper(getContactsController));
-router.get('contacts/contactById', ctrlWrapper(getContactByIdController));
-router.post('contacts', ctrlWrapper(createContactController));
-router.patch('contacts/contactById', ctrlWrapper(patchContactController));
-router.delete('/contacts/:contactById', ctrlWrapper(deleteContactController));
+router.get('/contacts/:contactId', ctrlWrapper(getContactByIdController));
+router.post('/contacts', ctrlWrapper(createContactController));
+router.patch('/contacts/:contactId', ctrlWrapper(patchContactController));
+router.delete('/contacts/:contactId', ctrlWrapper(deleteContactController));
 export default router;
